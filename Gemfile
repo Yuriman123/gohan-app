607459e3f5061fac34e71a6b781300c23dbb893d
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-05-10 13:28:08 +0900

    画像投稿

--- Gemfile
+++ Gemfile
@@ -55,3 +55,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'font-awesome-sass'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+gem 'pry-rails'
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,4 +10,19 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    @tweet = Tweet.new(tweet_parmas)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_parmas
+    params.require(:tweet).permit(:title, :catch_copy, :recipe, :image,)
+  end
+
 end
