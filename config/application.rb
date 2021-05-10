bd4bb3d4f74ae6c3fd5e48f9836c20f14738288e
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-05-10 16:55:00 +0900

    レイアウト変更

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,6 +11,7 @@ module GohanApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
