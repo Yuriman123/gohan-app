63484962ba8e6b5119fa895aaff1d03297e918f0
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-05-11 22:02:53 +0900

    showパスの設定とリンク

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
-
+  before_action :set_tweet, only: [:show,:edit,:destroy]
   
   def index
     @tweets = Tweet.includes(:user).order('created_at DESC')
@@ -19,10 +19,23 @@ class TweetsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
   private
 
   def tweet_parmas
     params.require(:tweet).permit(:title, :catch_copy, :recipe, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 
 end
