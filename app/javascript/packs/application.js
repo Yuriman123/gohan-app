cda602b0f273f3ecc88c64f13e821aa96b4134c2
Yuriman123 <76440720+Yuriman123@users.noreply.github.com>
2021-06-02 10:08:02 +0900

    詳細ページレイアウト変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,10 @@ import '@fortawesome/fontawesome-free/js/all';
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('../preview') //プレビュー機能
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
